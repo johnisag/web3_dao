72e7da7f9b5e53723cd0e0d6de2082e963843c86
Ioannis <agorgianitisj@hotmail.com>
2022-12-04 14:58:27 +0000

    update contract with meta api

--- nft_mcollection/hardhat_tutorial/constants/index.js
+++ nft_mcollection/hardhat_tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xa30214046Bdbed2fD0C4cA22736a65586b717a36";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-beta-two.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
