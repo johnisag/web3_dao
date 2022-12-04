b6542b65e61ea9874e6788d985fb52f151a177e8
Ioannis <agorgianitisj@hotmail.com>
2022-12-04 13:48:03 +0000

    nft collection front end

--- nft_collection/hardhat_tutorial/hardhat.config.js
+++ nft_collection/hardhat_tutorial/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// if error, in shell: npm install --save-dev @nomicfoundation/hardhat-toolbox 
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
